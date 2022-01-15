22eab84cfe6ede9fc3761e26ffa0183740a4bc00
satya <satya.p91@yahoo.com>
2022-01-15 14:08:12 +0530

    inital commit

---
+++ src/main/kotlin/com/satya/CryptoCC/CryptoCcApplication.kt
@@ -0,0 +1,11 @@
+package com.satya.CryptoCC
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CryptoCcApplication
+
+fun main(args: Array<String>) {
+	runApplication<CryptoCcApplication>(*args)
+}
---
+++ src/test/kotlin/com/satya/CryptoCC/CryptoCcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.satya.CryptoCC
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CryptoCcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
